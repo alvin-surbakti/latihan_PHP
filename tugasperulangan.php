d40d9c2e714c029f52c47171c739db809490d6b9
muhammad alvin <alvinajaaa416@gmail.com>
2025-09-30 14:29:33 +0700

    tugas acara 8

--- tugasperulangan.php
+++ tugasperulangan.php
@@ -15,24 +15,4 @@ while ($j <= 1000) {
     echo "Nilai: " . $j . "<br>";
     $j++;
 }
-echo "</div>";
-
-// Perulangan menggunakan DO WHILE
-echo "<h3>Perulangan dari 100 sampai 1000 (menggunakan DO WHILE)</h3>";
-echo "<div style='margin-left: 20px;'>";
-$k = 100;
-do {
-    echo "Nilai: " . $k . "<br>";
-    $k++;
-} while ($k <= 1000);
-echo "</div>";
-
-// Perulangan menggunakan FOREACH dengan array
-echo "<h3>Perulangan dari 100 sampai 1000 (menggunakan FOREACH)</h3>";
-echo "<div style='margin-left: 20px;'>";
-$numbers = range(100, 1000);
-foreach ($numbers as $number) {
-    echo "Nilai: " . $number . "<br>";
-}
-echo "</div>";
-?>
+echo "</div>
