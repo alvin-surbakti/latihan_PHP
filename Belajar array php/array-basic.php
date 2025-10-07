f5d1db2404b9a41f46772f91c8181031c238e08a
muhammad alvin <alvinajaaa416@gmail.com>
2025-10-07 13:47:44 +0700

    belajar array

---
+++ Belajar array php/array-basic.php
@@ -0,0 +1,12 @@
+<?php
+$punakawan = array("Semar","Gareng","Petruk","Bagong");
+echo $punakawan[0]; //Hasilnya Semar
+echo "<br>";
+echo $punakawan[3]; // Hasilnya Bagong
+echo "<br>";
+$punakawan[0]="Semar";
+$punakawan[1]="Gareng";
+$punakawan[2]="Petruk";
+$punakawan[3]="Bagong";
+echo $punakawan[3];
+?>
